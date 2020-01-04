fa5a634f67824bc14587d54bb132d5289b9fc180
Mateusz Staniak <mtst@mstaniak.pl>
2020-01-04 23:33:08 +0100

    adds code that gets the job done

--- big_data/1_data_download.R
+++ big_data/1_data_download.R
@@ -1,4 +1,3 @@
-install.packages('OpenML')
 library(OpenML)
 library(dplyr)
 library(ggplot2)
@@ -36,12 +35,23 @@ qsar_random <- getOMLDataSet(data.id = 3041)
 
 right_datasets <- oml_datasets %>%
     filter(number.of.numeric.features > 100) %>%
-    filter(number.of.numeric.features != 1025)
-
-oml_datasets_big <- vector('list', nrow(right_datasets))
+    filter(number.of.numeric.features != 1025) %>%
+    filter(number.of.features < 20000) %>%
+    filter(format == 'ARFF')
+max(right_datasets[['number.of.features']])
+ggplot(right_datasets, aes(x = number.of.features)) +
+    geom_histogram(binwidth = 50) +
+    theme_bw()
+# table(right_datasets[['number.of.features']])
+# right_datasets %>%
+#     filter(number.of.features == 10937)
+# clearOMLCache()
 for(i in 1:nrow(right_datasets)) {
-    try({
-        oml_datasets[[i]] <- getOMLDataSet(data.id = right_datasets[['data.id']][i])
+    tryCatch({
+        dataset <- getOMLDataSet(data.id = right_datasets[['data.id']][i])
         print(paste0('PULL okay: ', i))
-    }, silent = TRUE)
+        saveRDS(dataset, file = paste0('./data/dataset_', i, ".RDS"))
+        rm(dataset)
+        clearOMLCache()
+    }, error = function(e) print(paste0('PULL failed: ', i, ' ', e)))
 }    
